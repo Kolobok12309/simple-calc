e539985f321658b0604debef0f49b087156f7777
Савинов Дмитрий <sova123409@gmail.com>
2019-12-18 12:41:21 +0300

    Add styles and buttons

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ const findAction = (id) => actions.find(action => action.id === id);
 class Calc {
     // Задание дефолтных значений
     constructor(elem) {
+        this.listeners = {
+            nums: {},
+            actions: {},
+        };
         this.elem = elem;
         this._firstNum = '';
         this._secondNum = '';
@@ -94,6 +98,8 @@ class Calc {
 
     // Установка действия
     setAction(actionId) {
+        if (actionId === consts.actions.EQUAL) return this.result();
+
         const action = findAction(actionId);
         if (!action) throw new CalcError('Undefined action');
 
@@ -153,6 +159,7 @@ class Calc {
             // Если нажата равно, но действий еще не было, то ничего не делать
             if (this.lastArgs.action === consts.actions.EQUAL) return;
 
+            // Если предыдущее действие все же было, то используем его и аргумент его же
             actionId = this.lastArgs.action;
             secondNum = nums[2];
         }
@@ -181,11 +188,76 @@ class Calc {
     // Добавление символов к числам к первому или второму
     // Схоже с предыдущим, но не заменяет, а дополняет число
     addChars(char) {
-        if (this.nowAction === consts.actions.EQUAL) this.firstNum += char;
-        else this.secondNum += char;
+        if (this.nowAction === consts.actions.EQUAL) this.firstNum += String(char);
+        else this.secondNum += String(char);
+    }
+
+    // Получить функцию для использования в слушателях действий
+    getActionListener(actionId) {
+        const actions = this.listeners.actions;
+        // Проверка была ли уже создана функция
+        if (actions[actionId]) return actions[actionId];
+
+        // Создание нового слушателя
+        const newListener = () => this.setAction(actionId);
+        // Сохранения слушателя, чтобы в случае чего
+        // можно было удалить используя ту же функцию
+        actions[actionId] = newListener;
+
+        return newListener;
+    }
+
+    // Аналогично предыдущему, но для ввода чисел
+    getNumListener(char) {
+        const nums = this.listeners.nums
+        if (nums[char]) return nums[char];
+
+        const newListener = () => this.addChars(char);
+        nums[char] = newListener;
+
+        return newListener;
     }
 }
 
 const elem = document.getElementById('calc__display');
 
 const calc = new Calc(elem);
+
+for (let i = 0; i < 10; i++) {
+    const buttonElem = document.getElementById(`calc__num__${i}`);
+
+    buttonElem.addEventListener('click', calc.getNumListener(i));
+}
+document.getElementById('calc__num__dot').addEventListener('click',  calc.getNumListener('.'));
+
+const actionsForListeners = [
+    {
+        id: consts.actions.EQUAL,
+        htmlId: 'equal'
+    },
+    {
+        id: consts.actions.MINUS,
+        htmlId: 'minus',
+    },
+    {
+        id: consts.actions.PLUS,
+        htmlId: 'plus'
+    },
+    {
+        id: consts.actions.DIV,
+        htmlId: 'div'
+    },
+    {
+        id: consts.actions.MUL,
+        htmlId: 'mul'
+    },
+    {
+        id: consts.actions.SQRT,
+        htmlId: 'sqrt'
+    },
+];
+
+actionsForListeners.forEach(({ id, htmlId }) => {
+    const elem = document.getElementById(`calc__buttons__${htmlId}`);
+    elem.addEventListener('click', calc.getActionListener(id));
+})
